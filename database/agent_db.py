8fe4e7ddbd3ebbbf466e6b3c873e88865176fcc3
Meir <meirroc5764@gmail.com>
2026-06-17 17:50:16 +0300

    start class AgentDB with two method

---
+++ database/agent_db.py
@@ -0,0 +1,90 @@
+from pydantic import BaseModel
+from typing import Literal
+
+from db_connection import DBConnection
+
+
+
+dbc = DBConnection()
+
+class AgentDB:
+    def __init__(self):
+        pass
+
+    def create_agent(self,data):
+        conn = None
+        try:    
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("INSERT INTO agents(name, specialty, agent_rank) VALUES(%s,%s,%s)",(data["name"],data["specialty"],data["agent_rank"]))
+
+            conn.commit()
+
+            return "create a agent"
+        
+        except Exception as e:
+            raise e
+    
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+    
+    def get_all_agents(self):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM agents")
+
+            all_data = cursor.fetchall()
+
+            return all_data
+        
+        except Exception as e:
+            raise e
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+
+    def get_agent_by_id(self,id):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM agents WHERE id = %s",(id,))
+
+            my_agent = cursor.fetchone()
+
+            return my_agent
+        
+        except Exception as e:
+            raise e
+        
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    
+
+
+
+if __name__=="__main__":
+    adb = AgentDB()
+    agent = {"name":"meir",
+             "specialty":"program",
+             "agent_rank":"junior"}
+    # print(adb.create_agent(agent))
+    print(adb.get_all_agents())
+    # print(adb.get_agent_by_id(2))
+
