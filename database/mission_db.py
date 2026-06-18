0eefa176f869168d7eed51de0b1669a36cffa129
Meir <meirroc5764@gmail.com>
2026-06-18 12:45:30 +0300

    finish class MissionDB

--- database/mission_db.py
+++ database/mission_db.py
@@ -12,6 +12,18 @@ class MissionDB:
 
         my_risk_level = difficulty * 2 + importance
 
+        if 0 <= my_risk_level < 10:
+            my_risk_level = "LOW"
+        
+        elif 10 <= my_risk_level < 18:
+            my_risk_level = "MEDIUM"
+        
+        elif 18 <= my_risk_level < 25:
+            my_risk_level = "HIGH"
+        
+        elif my_risk_level >= 25:
+            my_risk_level = "CRITICAL"
+
         return my_risk_level
 
 
@@ -106,6 +118,8 @@ class MissionDB:
 
             conn.commit()
 
+            self.update_mission_status(m_id,"ASSIGNED")
+
             return "a mission assign"
         
         except Exception as e:
@@ -139,7 +153,151 @@ class MissionDB:
     
     
     
+    def get_open_missions_by_agent(self,id):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM missions WHERE assigned_agent_id = %s AND status IN (%s,%s)",(id,"ASSIGNED","IN_PROGRESS"))
+
+            my_mission = cursor.fetchall()
+
+            if not my_mission:
+                return None
+
+            return my_mission
+        
+        
+        except Exception as e:
+            raise e
+        
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
     
+
+    def count_all_missions(self):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT COUNT(*) FROM missions")
+
+            all_data = cursor.fetchone()[0]
+
+            return all_data
+        
+        except Exception as e:
+            raise e
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+    
+    
+    def count_by_status(self,status):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT COUNT(*) FROM missions WHERE status = %s",(status,))
+
+            all_data = cursor.fetchone()[0]
+
+            return all_data
+        
+        except Exception as e:
+            raise e
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+    
+    
+    def count_open_missions(self):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT COUNT(*) FROM missions WHERE status = %s OR status = %s",("ASSIGNED","IN_PROGRESS"))
+
+            all_data = cursor.fetchone()[0]
+
+            return all_data
+        
+        except Exception as e:
+            raise e
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+
+    def count_critical_missions(self):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT COUNT(*) FROM missions WHERE status = %s",("CRITICAL",))
+
+            all_data = cursor.fetchone()[0]
+
+            return all_data
+        
+        except Exception as e:
+            raise e
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+    
+
+    def get_top_agent(self):
+        conn = None
+        try:
+            conn = dbc.get_connection()
+
+            cursor = conn.cursor(dictionary=True)
+
+            cursor.execute("SELECT MAX(completed_missions) AS max FROM agents")
+
+            max_num = cursor.fetchone()["max"]
+
+            cursor.execute("SELECT * FROM agents WHERE completed_missions = %s",(max_num,))
+
+            best_agent = cursor.fetchone()
+
+            return best_agent
+        
+        except Exception as e:
+            raise e
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+
+
+
+
+
+
 
 
 if __name__=="__main__":
@@ -149,7 +307,15 @@ if __name__=="__main__":
              "detion":"junior",
              "difficulty":5,
              "importance":6}
+    # print(mdb.risk_level(5,6))
     # print(mdb.create_mission(missions))
     print(mdb.get_all_missions())
-    print(mdb.get_mission_by_id(2))
-    print(mdb.assign_mission(1,5))
+    # print(mdb.get_mission_by_id(2))
+    # print(mdb.assign_mission(1,5))
+    pr
+    print(mdb.get_open_missions_by_agent(5))
+    # print(mdb.count_all_missions())
+    # print(mdb.count_by_status("new"))
+    # print(mdb.count_open_missions())
+    # print(mdb.count_critical_missions())
+    # print(mdb.get_top_agent())
